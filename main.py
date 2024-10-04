b6c1a3d44d5ab3a9b79ea5641e8593dc0db25a17
avihaz <avihazan22@gmail.com>
2024-10-04 17:22:17 +0300

    stable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-from IPython.display import HTML
 from matplotlib.animation import ArtistAnimation
 
 import nengo
@@ -12,13 +10,15 @@ nengo_loihi.set_defaults()
 
 rng = np.random.RandomState(0)
 
-t_length = 1
+t_length = 5
 
 events_file_name = r'C:\Users\USER\PycharmProjects\nengox\data\dvs-from-file-events.events'
 
 if __name__ == '__main__':
 
-    with nengo.Network() as net:
+    model = nengo.Network(label="NEF summary")
+
+    with model:
 
         print("Creating DVS object")
         dvs_process = nengo_loihi.dvs.DVSFileChipProcess(
@@ -33,7 +33,7 @@ if __name__ == '__main__':
         positive_ensemble = nengo.Ensemble(
             dvs_process.height * dvs_process.width,
             1,
-            neuron_type=nengo.SpikingRectifiedLinear(),
+            neuron_type=nengo.LIF(tau_rc=0.02, tau_ref=0.002),
             gain=nengo.dists.Choice([101]),
             bias=nengo.dists.Choice([0]),
         )
@@ -41,7 +41,7 @@ if __name__ == '__main__':
         negative_ensemble = nengo.Ensemble(
             dvs_process.height * dvs_process.width,
             1,
-            neuron_type=nengo.SpikingRectifiedLinear(),
+            neuron_type=nengo.LIF(tau_rc=0.02, tau_ref=0.002),
             gain=nengo.dists.Choice([101]),
             bias=nengo.dists.Choice([0]),
         )
@@ -52,10 +52,10 @@ if __name__ == '__main__':
         nengo.Connection(u[0:: dvs_process.polarity], positive_ensemble.neurons, transform=1.0)
         nengo.Connection(u[1:: dvs_process.polarity], negative_ensemble.neurons, transform=1.0)
 
-        probes = [nengo.Probe(positive_ensemble.neurons), nengo.Probe(negative_ensemble.neurons)]
+        probes = [nengo.Probe(negative_ensemble.neurons),nengo.Probe(positive_ensemble.neurons)]
 
     print("Running sim")
-    with nengo_loihi.Simulator(net,progress_bar=True) as sim:
+    with nengo.Simulator(model) as sim:
         sim.run(t_length)
 
     sim_t = sim.trange()
@@ -63,8 +63,10 @@ if __name__ == '__main__':
     output_spikes_neg = sim.data[probes[0]].reshape(shape) * sim.dt
     output_spikes_pos = sim.data[probes[1]].reshape(shape) * sim.dt
 
+
     dt_frame = 0.01
     t_frames = dt_frame * np.arange(int(round(t_length / dt_frame)))
+
 
     fig = plt.figure()
     imgs = []
@@ -76,7 +78,12 @@ if __name__ == '__main__':
         frame_img = np.zeros((dvs_process.height, dvs_process.width))
         frame_img -= output_spikes_neg[m].sum(axis=0)
         frame_img += output_spikes_pos[m].sum(axis=0)
-        # frame_img = frame_img / np.abs(frame_img).max()
+
+        min_val = frame_img.min()
+        max_val = frame_img.max()
+
+        if max_val != min_val:  # Avoid division by zero if all values are the same
+            frame_img = 2 * (frame_img - min_val) / (max_val - min_val) - 1  # Normalize to range [-1, 1]
 
         img = plt.imshow(frame_img, vmin=-1, vmax=1, animated=True)
         imgs.append([img])
